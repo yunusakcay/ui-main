33cba0165010f66f9679252dab861a18037fd94c
Yunus Akçay <153110603+yunusakcay@users.noreply.github.com>
2025-03-08 10:27:19 +0300

    login-01

---
+++ login-01/main.js
@@ -0,0 +1,5 @@
+function togglePassword(button) {
+  button.classList.toggle("showing");
+  const input = document.getElementById("password");
+  input.type = input.type === "password" ? "text" : "password";
+};
